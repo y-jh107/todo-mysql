fef0473b8ff6c824f65e018bf8de199ecca7bb7e
양진혁 <jin99545@gmail.com>
2026-02-14 23:55:03 +0900

    refactor: Swagger 설정

--- src/main/java/com/yjh107/todo/config/ApiDocumentationConfig.java
+++ src/main/java/com/yjh107/todo/config/ApiDocumentationConfig.java
@@ -14,7 +14,7 @@ public class ApiDocumentationConfig {
                 .info(
                         new Info()
                                 .title("TODO List API")
-                                .version("1.0")
+                                .version("2.0")
                                 .description("Spring Boot3를 이용한 TODO List API 문서"));
     }
 }
